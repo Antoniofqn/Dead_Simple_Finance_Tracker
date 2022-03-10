5ed4c02f48d309d742068ce710b468834d09e19f
Antonio Florencio <antoniofqn@hotmail.com>
2022-03-10 17:25:20 -0300

    added link to income transaction creation in the user page. Need to make it a pop up but its working for now. Expense buttom not working still. Will look into that

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: %i[show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :transactions, only: [:new, :create]
 end
