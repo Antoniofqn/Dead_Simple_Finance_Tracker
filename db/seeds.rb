e6104dd61d8c21b2d3b98fd09ab8fc9724aa0777
Antonio Florencio <antoniofqn@hotmail.com>
2022-03-11 10:05:54 -0300

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-type = ["income", "expense"]
+type = ["Income", "Expense"]
 date = Date.new(2022, 03, [1,2,3].sample)
 value = (50..100).to_a
-category = ["food", "home", "pleasure"]
+category_expense = ["Rent", "Mortgage", "House Bills", "Insurance", "Health", "Groceries", "Fuel", "Interest", "Leisure"]
+category_income = ["Salary", "Dividends", "Bonus", "Tax Refund", "Gift", "Other"]
 
 
 
 10.times do
   puts "creating now"
-  transaction = Transaction.new(type_transaction: type.sample, date: date, value: value.sample, category: category.sample, user_id: 1)
+  transaction = Transaction.new(type_transaction: type.sample, date: date, value: value.sample, user_id: 1)
+  if transaction.type_transaction == "Expense"
+    transaction.category = category_expense.sample
+  else
+    transaction.category = category_income.sample
+  end
   if transaction.save
     puts "saved!"
   end
