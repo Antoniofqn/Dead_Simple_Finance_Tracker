03be8daf6412365d2a277b054694eab8a1fa0351
Antonio Florencio <antoniofqn@hotmail.com>
2022-03-17 13:55:40 -0300

    made changes on show page to reflect current goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,10 +4,16 @@ class GoalsController < ApplicationController
     @goal = Goal.new(goal_params)
     @goal.user = current_user
     if @goal.save
-      redirect_to user_path
+      redirect_to user_path(current_user)
     else
       render partial: "form_goal"
     end
   end
 
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:name, :objective, :current_value, :achieved, :cancelled)
+  end
 end
