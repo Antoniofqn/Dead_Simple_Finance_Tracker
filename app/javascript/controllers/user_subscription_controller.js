95fc53de72e5b5d007d9addf962606f82b03a342
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-18 17:30:03 -0300

    Stocks Almost done

--- app/javascript/controllers/user_subscription_controller.js
+++ app/javascript/controllers/user_subscription_controller.js
@@ -1,14 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-import consumer from "../channels/consumer"
 
-const socket = new WebSocket("wss://ws.twelvedata.com/v1/quotes/price?apikey=c6562a12261f45b2862ac84916e34674");
+let socket = new WebSocket("wss://ws.twelvedata.com/v1/quotes/price?apikey=c6562a12261f45b2862ac84916e34674");
 
 export default class extends Controller {
-  static values = { userId: Number }
   static targets = ["stocks"]
 
   connect() {
+    const insertData = (data, stocks = this.stocksTarget) => {
+      stocks.querySelector(`.${(data.symbol).toLowerCase()} .stock-value`).innerText = `$${data["price"].toFixed(3)}`
+    }
+    console.log(socket)
     socket.onopen = function(e) {
+      console.log(socket)
       console.log("Connection established");
       socket.send(`{
         "action": "subscribe",
@@ -18,12 +21,16 @@ export default class extends Controller {
         }`);
     };
     socket.onmessage = function(event) {
-      console.log(`[message] Data received from server: ${event.data}`);
+      let data = JSON.parse(event.data)
+      console.log(data);
+      insertData(data)
     };
   }
 
   disconnect() {
     socket.close()
+    socket = new WebSocket("wss://ws.twelvedata.com/v1/quotes/price?apikey=c6562a12261f45b2862ac84916e34674");
     console.log("Unsubscribed from the chatroom")
   }
+
 }
