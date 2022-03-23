23e47ee6d7cef408a27ba8e65b5710883d5b6bc4
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-23 10:54:58 -0300

    Removed console log spam

--- app/javascript/controllers/user_subscription_controller.js
+++ app/javascript/controllers/user_subscription_controller.js
@@ -11,10 +11,7 @@ export default class extends Controller {
     const insertData = (data, stocks = this.stocksTarget) => {
       stocks.querySelector(`.${(data.symbol).toLowerCase().replace(/\W/, "")} .stock-value`).innerText = `$${data["price"].toFixed(3)}`
     }
-    console.log(socket)
     socket.onopen = function(e) {
-      console.log(socket)
-      console.log("Connection established");
       socket.send(`{
         "action": "subscribe",
         "params": {
@@ -23,8 +20,7 @@ export default class extends Controller {
         }`);
     };
     socket.onmessage = function(event) {
-      let data = JSON.parse(event.data)
-      console.log(data);
+      let data = JSON.parse(event.data);
       insertData(data)
     };
   }
@@ -32,7 +28,6 @@ export default class extends Controller {
   disconnect() {
     socket.close()
     socket = new WebSocket("wss://ws.twelvedata.com/v1/quotes/price?apikey=c6562a12261f45b2862ac84916e34674");
-    console.log("Unsubscribed from the chatroom")
   }
 
 }
